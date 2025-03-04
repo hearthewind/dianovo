3a78ce8f479647d71c9a7b99d6888749c1348ea8
hearthewind <winston.zheng.m@gmail.com>
2025-03-03 22:04:00 -0500

    organize, add more readme

--- denovo_from_feature_detection/denovo_op_386on453.sh
+++ denovo_from_feature_detection/denovo_op_386on453.sh
@@ -3,5 +3,5 @@ serialized_model_path=save/ckpt \
 gnova_file_name=GNova-Astral_pretrain-453 \
 wandb.project=RNova-Astral wandb.name=op-453 \
 infer.optimal_path_file=prediction/optimal_path/386on453.csv \
-infer.dataset_dir=/home/m/data/Dropbox/shared/dianovo_data/generated_data_46386/ \
+infer.dataset_dir=/home/m/data/Dropbox/shared/dianovo_data/denovo_data_46386/ \
 infer.test_spec_header_filename=02052023_Yeast_KO_3Th_2p5ms_KO1_rep01_20230503212731_2.csv
--- denovo_from_feature_detection/utils/gen_test.sh
+++ denovo_from_feature_detection/utils/gen_test.sh
@@ -3,4 +3,4 @@
 sh parallel_preprocessing.sh 2 \
 ~/data/Dropbox/shared/dianovo_data/raw_data_46386/02052023_Yeast_KO_3Th_2p5ms_KO1_rep01_20230503212731.mzML \
 ~/data/Dropbox/shared/dianovo_data/raw_data_46386/02052023_Yeast_KO_3Th_2p5ms_KO1_rep01_20230503212731_PeakCluster.csv \
-~/data/Dropbox/shared/dianovo_data/generated_data_46386/
+~/data/Dropbox/shared/dianovo_data/denovo_data_46386/
